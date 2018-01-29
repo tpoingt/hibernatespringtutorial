cd4a5cf21922cf58c9082b7cd4687f162f1d056d
Teddy Poingt <tpoingt@gmail.com>
2018-01-29 18:46:41 -0500

    First commit

---
+++ src/main/java/ca/effenti/tutorial/hibernate/hibernatespringtutorial/HibernateSpringTutorialApplication.java
@@ -0,0 +1,12 @@
+package ca.effenti.tutorial.hibernate.hibernatespringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringTutorialApplication.class, args);
+	}
+}
